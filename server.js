7788da5fd2fad3035ea39f14db4031b8bbc4820a
Mustafa Murat Coşkun <coskun.m.murat@gmail.com>
2019-11-03 15:19:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get("/api/products", (req, res) => {
   res.json([
     { id: 1, name: "Laptop", price: 3000 },
