32dd2656f6ac1724fd81d72e639212485c92ce10
Mustafa Murat Coşkun <coskun.m.murat@gmail.com>
2019-11-03 11:58:52 +0300

    Modifications - server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
 app.get("/api/products", (req, res) => {
   res.json([
     { id: 1, name: "Laptop", price: 3000 },
